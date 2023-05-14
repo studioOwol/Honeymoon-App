6ab1e729d4cded318458e514343649aa7b0687d0
studioOwol <from.owol@gmail.com>
2023-05-14 20:56:28 +0900

    Binding - showing alert

--- Honeymoon/View/FooterView.swift
+++ Honeymoon/View/FooterView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct FooterView: View {
+    //MARK: - Properties
+    @Binding var showBookingAlert: Bool
+    
     var body: some View {
         HStack {
             Image(systemName: "xmark.circle")
@@ -17,7 +20,8 @@ struct FooterView: View {
             
             Button(action: {
                 // Action
-                print("Success!")
+                // print("Success!")
+                self.showBookingAlert.toggle()
             }) {
                 Text("Book Destination".uppercased())
                     .font(.system(.subheadline, design: .rounded))
@@ -40,8 +44,10 @@ struct FooterView: View {
 }
 
 struct FooterView_Previews: PreviewProvider {
+    @State static var showAlert: Bool = false
+    
     static var previews: some View {
-        FooterView()
+        FooterView(showBookingAlert: $showAlert)
             .previewLayout(.fixed(width: 375, height: 80))
     }
 }
