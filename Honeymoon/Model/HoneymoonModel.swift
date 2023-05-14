dcff4455200a733765853a13578972699a99bedc
studioOwol <from.owol@gmail.com>
2023-05-14 20:24:46 +0900

    Card View

---
+++ Honeymoon/Model/HoneymoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneymoonModel.swift
+//  Honeymoon
+//
+//  Created by Nahyun on 2023/05/14.
+//
+
+import SwiftUI
+
+struct Destination {
+    var place: String
+    var country: String
+    var image: String
+}
