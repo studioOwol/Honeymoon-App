34f7e33d132c90af8b8adc5c8615338de2521d37
studioOwol <from.owol@gmail.com>
2023-05-14 20:07:45 +0900

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Nahyun on 2023/05/14.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
